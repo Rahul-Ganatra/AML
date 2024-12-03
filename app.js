09d1c387f55b1a0c3f3c4d8d5bb88c6506d9c44f
Rahul-Ganatra <rahul.ganatra@somaiya.edu>
2024-12-03 16:22:11 +0530

    user FE

---
+++ app.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0lMVVq_q-Pjd-R9VnnN-e9Sdp556qDKQ",
+  authDomain: "anti-money-landering.firebaseapp.com",
+  projectId: "anti-money-landering",
+  storageBucket: "anti-money-landering.firebasestorage.app",
+  messagingSenderId: "775468075770",
+  appId: "1:775468075770:web:1ffe61139ec2147d5ed743",
+  measurementId: "G-2FGQ872XXL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
